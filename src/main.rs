b70a78397a6487cf472de8f6762ed553d1e843e1
Nitesh Kumar Debnath <30214255+veechiecooqu@users.noreply.github.com>
2023-04-17 14:36:56 +0530

    Fixes identity crisis

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::{env, io};
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        eprintln!("Usage: edit_file <file_path>");
+        eprintln!("Usage: DoasEdit <file_path>");
         std::process::exit(1);
     }
 
